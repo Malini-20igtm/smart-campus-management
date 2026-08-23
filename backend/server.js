5e01a34d8ccd4fcf0f0a210204133a6f37a056e9
malini-20igtm <musumalini@gmail.com>
2026-08-23 11:47:19 +0530

    Complete Day 5 backend API integration

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get('/api/campus', (req, res) => {
+  res.json({
+    college: 'Smart Campus College',
+    students: 500,
+    faculty: 50,
+    departments: 8
+  });
+});
 
 app.get('/', (req, res) => {
   res.json({
@@ -13,6 +21,7 @@ app.get('/', (req, res) => {
   })
 })
 const PORT = 5000;
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 function App() {
-  const [message, setMessage] = useState("");
-  const getMessage = async () => {
-    const response = await fetch("http://localhost:5000/");
-    const data = await response.json();
-    setMessage(data.message);
-
-  };
+  const [campus, setCampus] = useState(null);
+  const getCampus = async () => {
+  const response = await fetch("http://localhost:5000/api/campus");
+  const data = await response.json();
+  setCampus(data);
+};
+  
 
   return (
     <div>
       <h1>Smart Campus Management</h1>
 
-      <button onClick={getMessage}>
-        Connect to Backend
-      </button>
-      <p>{message}</p>
+      <button onClick={getCampus}>
+        Get Campus Data
+        </button>
+      {campus && (
+        <div>
+          <p>College: {campus.college}</p>
+          <p>Students: {campus.students}</p>
+          <p>Faculty: {campus.faculty}</p>
+          <p>Departments: {campus.departments}</p>
+        </div>
+      )}
     </div>
   );
 }
