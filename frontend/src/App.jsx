56409c60593ad5447a3d95a71bcc471bf3e80eaa
malini-20igtm <musumalini@gmail.com>
2026-08-29 19:30:07 +0530

    Day 6: Added student module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+
 function App() {
   const [campus, setCampus] = useState(null);
+
   const getCampus = async () => {
-  const response = await fetch("http://localhost:5000/api/campus");
-  const data = await response.json();
-  setCampus(data);
-};
-  
+    const response = await fetch("http://localhost:5000/api/campus");
+    const data = await response.json();
+    setCampus(data);
+  };
+
+  const student = {
+    name: "Malini",
+    studentId: "ST101",
+    department: "CSE",
+    attendance: "85%"
+  };
 
   return (
     <div>
@@ -14,7 +22,8 @@ function App() {
 
       <button onClick={getCampus}>
         Get Campus Data
-        </button>
+      </button>
+
       {campus && (
         <div>
           <p>College: {campus.college}</p>
@@ -23,6 +32,15 @@ function App() {
           <p>Departments: {campus.departments}</p>
         </div>
       )}
+
+      <hr />
+
+      <h2>Student Details</h2>
+
+      <p>Name: {student.name}</p>
+      <p>Student ID: {student.studentId}</p>
+      <p>Department: {student.department}</p>
+      <p>Attendance: {student.attendance}</p>
     </div>
   );
 }
